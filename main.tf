a76225313fef6bbe62a451d357eb95d89d18e8dd
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2026-04-15 11:28:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"   # change if needed
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_ec2" {
@@ -17,12 +17,29 @@ resource "aws_instance" "my_ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
-
               yum update -y
-              yum install -y docker
 
+              # Install Docker and Git
+              yum install -y docker git
+
+              # Start Docker
               systemctl start docker
               systemctl enable docker
+
+              # Give Docker some time to start
+              sleep 10
+
+              # Clone your repository
+              cd /home/ec2-user
+              git clone https://github.com/suysuyua/practice.git
+
+              cd practice
+
+              # Build Docker image
+              docker build -t studentimage .
+
+              # Run container
+              docker run -d -p 5000:5000 --name mystudent studentimage
               EOF
 
   tags = {
