6fc46516ef8be3cfdbef1cbd1a34832736098924
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2026-04-17 15:52:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,39 +14,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0e670eb768a5fc3d4" # Amazon Linux 2
   instance_type = "t2.micro"
-
-  user_data = <<-EOF
-#!/bin/bash
-
-# update system
-yum update -y
-
-# install docker & git
-yum install -y docker git
-
-# start docker
-systemctl start docker
-systemctl enable docker
-
-# allow ec2-user to use docker
-usermod -aG docker ec2-user
-
-# go to ec2-user home
-cd /home/ec2-user
-
-# clone your repo
-git clone -b Dev https://github.com/vamsichilaka/practice.git
-
-# go inside repo
-cd practice
-
-# build docker image
-docker build -t studentapp:v1 .
-
-# run container
-docker run -d -p 8081:5000 --name studentdetails studentapp:v1
-
-EOF
+  key_name = "terraform"
 
   tags = {
     Name = "myec2"
